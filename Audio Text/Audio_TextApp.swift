1890f88852d57642659f5ffde469c37a35ee7976
Steve Tom <tomsteve187@gmail.com>
2025-07-03 14:32:45 +0300

    Initial Commit

---
+++ Audio Text/Audio_TextApp.swift
@@ -0,0 +1,32 @@
+//
+//  Audio_TextApp.swift
+//  Audio Text
+//
+//  Created by raccoon on 03/07/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Audio_TextApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
